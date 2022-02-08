d1f282d342e99a604c046f0749ba3df86a42ead1
Julius Garrido <jflgarrido@gmail.com>
2022-02-08 12:42:05 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(r => r.json())
+    .then(cards => setCards(cards))
+  }, [])
+
+  function handleDeleteACard(deletedCard){
+    const updatedCardList = cards.filter((card) => {
+      return card.id === !deletedCard
+    })
+    setCards(updatedCardList)
+  }
+
+  const displayedCards = cards.filter((card) => {
+    return card.description.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch} />
+      <ListingsContainer 
+        cards={displayedCards} 
+        onDeleteACard={handleDeleteACard}/>
     </div>
   );
 }
